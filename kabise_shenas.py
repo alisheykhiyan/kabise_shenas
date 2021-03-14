49f97b7dd3091ed9957781224b46d59f14a74abf
ali sheykhiyan <80154787+alisheykhiyan@users.noreply.github.com>
2021-03-14 09:47:35 +0330

    Update kabise_shenas.py

--- kabise_shenas.py
+++ kabise_shenas.py
@@ -1,6 +1,6 @@
 # kabise_shenas.py
 considered_year = int(input("Sal mad nazar khod ra jahat barresi vared namaeed :"))
-if ((1399-considered_year)/4) = 1 or -1:
+if ((1399-considered_year)/4) = 1 or -1 or 0:
     print ("Emsal sal kabise ast.")
 if  ((1399-considered_year)/4) = 1/4 or -1/4:
     print ("Emsal sal kabise nist .Sal kabise badi 3 sal digar khahad bood")
